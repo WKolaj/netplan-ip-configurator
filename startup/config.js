32cc5e15c44b504768d8091430b5c66072467950
WKolaj <WKolaj@gmail.com>
2020-12-27 17:48:49 +0100

    Fixed issue with check config file - netplan apply command

--- startup/config.js
+++ startup/config.js
@@ -19,6 +19,7 @@ module.exports = async function () {
   throwIfConfigDoesNotExist("socketUserGroupID");
   throwIfConfigDoesNotExist("socketDirPath");
   throwIfConfigDoesNotExist("appAuthToken");
+  throwIfConfigDoesNotExist("netplanApplyCommand");
 
   //#endregion ========== CUSTOM CONFIG PROPERTIES ==========
 
